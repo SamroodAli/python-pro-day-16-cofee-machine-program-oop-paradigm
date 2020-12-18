7b575fc46f9f5ecf80e47c15e3de56d631631a70
SamroodAli <samroodali@live.com>
2020-12-19 00:38:16 +0530

    Add:loop for coffee machine

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from logo import LOGO
 
-coffee_maker = CoffeeMaker()
+# variables
+menu = Menu()
+menu_items = menu.get_items().replace("/", " ")
+is_on = True
+while is_on:
+    print(LOGO)
+    print("What would you like ?\n",menu_items)
+    user_input = input()
+    is_on = False
